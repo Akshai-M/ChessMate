6c61b56ccf929bcd6e7ec05a57f81f09fc7afc95
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-07-24 19:34:35 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 
 const config = require('../config');
 
+const myIo = require('./sockets/io'),
+      routes = require('./routes/routes');
+
 
 console.log(`http://localhost:${config.port}`);
 
 const Handlebars = handlebars.create({
- 
+  extname: '.html', 
+  partialsDir: path.join(__dirname, '..', 'front', 'views', 'partials'), 
+  defaultLayout: false,
+  helpers: {}
 });
