9efcd2d2052b99d96a351060c88818c079df3c49
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-08-22 11:39:56 +0530

    socketio

---
+++ server/sockets/io.js
@@ -0,0 +1,25 @@
+module.exports = io => {
+    io.on('connection', socket => {
+        console.log('New socket connection');
+
+        let currentCode = null;
+
+        socket.on('move', function(move) {
+            console.log('move detected')
+
+            io.to(currentCode).emit('newMove', move);
+        });
+        
+        
+
+        socket.on('disconnect', function() {
+            console.log('socket disconnected');
+
+            if (currentCode) {
+                io.to(currentCode).emit('gameOverDisconnect');
+                delete games[currentCode];
+            }
+        });
+
+    });
+};
