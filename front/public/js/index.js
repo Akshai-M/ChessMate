5df469d8df75c1db4f29dca863a3b7771738a380
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-02 22:45:14 +0530

    index

--- front/public/js/index.js
+++ front/public/js/index.js
@@ -1,6 +1,15 @@
+
 let gameHasStarted = false;
 var board = null
+var game = new Chess()
+var $status = $('#status')
+var $pgn = $('#pgn')
+let gameOver = false;
 
 function onDragStart(source, piece, position, orientation) {
-    
+   
+    // only pick up pieces for the side to move
+    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) || (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        return false
+    }
 }
